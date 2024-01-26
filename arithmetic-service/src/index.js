1f986553d466de5afed6ab2c022634c0cde4d94d
adamshm <adamshm@iu.edu>
2024-01-25 20:42:39 -0500

    practicum2

---
+++ arithmetic-service/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+})
+
+app.listen(port);
