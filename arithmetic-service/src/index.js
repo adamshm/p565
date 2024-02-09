01d1d6fc5068a6b04e2a4634c31a7bba1ecec003
adamshm <adamshm@iu.edu>
2024-02-08 19:56:36 -0500

    pract4

--- arithmetic-service/src/index.js
+++ arithmetic-service/src/index.js
@@ -3,8 +3,17 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const port = 3000;
+
 app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello World!');
+});
+
+app.get('/add/:num1/:num2', (req, res) => {
+    const num1 = parseFloat(req.params.num1);
+    const num2 = parseFloat(req.params.num2);
+    const sum = num1 + num2;
+
+    res.json(sum);
 })
 
 app.listen(port);
